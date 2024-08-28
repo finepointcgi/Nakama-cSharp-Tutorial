9011b8d781763a5a9db3db4698b3512454ccd27b
Mitch <mitchelldmccollum@gmail.com>
2024-08-27 22:07:19 -0400

    messages

---
+++ ChatChannel.cs
@@ -0,0 +1,5 @@
+public class ChatChannel
+{
+    public string ID;
+    public string Label;
+}
---
+++ ChatMessage.cs
@@ -0,0 +1,7 @@
+public class ChatMessage
+{
+    public string Message;
+    public string User;
+    public string ID;
+    public int Type;
+}
